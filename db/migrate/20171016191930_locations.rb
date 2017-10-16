4f0266963008d783904e70b79633d071f618e492
Cecilia Wong <ceciliakhwong@gmail.com>
2017-10-16 15:23:12 -0400

    Create migration for party_guests table, create migration for locations table

---
+++ db/migrate/20171016191930_locations.rb
@@ -0,0 +1,9 @@
+class Locations < ActiveRecord::Migration[5.0]
+  def change
+    create_table :locations do |t|
+      t.string  :name
+      t.text    :address
+      t.integer :capacity
+    end
+  end
+end
